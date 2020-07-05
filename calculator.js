84a2f548a800e148fdcc471b3c9f8883da88ca1e
NingenTech <59954804+NingenTec@users.noreply.github.com>
2020-07-05 16:34:13 -0700

    Add the logic for the calculator and route it to the index.html file for result

--- calculator.js
+++ calculator.js
@@ -1,15 +1,23 @@
 //jshint esversion:6
 
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
-    
+app.use(bodyParser.urlencoded({extended: true})); 
+
 app.get("/", function(_req, res){
 res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", function(_req, res){
-    res.send("Thanks for posting that!")
+
+var num1 = Number(_req.body.n1);
+var num2 = Number(_req.body.n2);
+
+var result = num1 + num2;
+
+    res.send("The result of the calculation is " + result);
 });
 
 app.listen(3000, function(){
