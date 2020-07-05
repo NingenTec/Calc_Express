c564e5fe851e6f3163e2188ab954e93137f0c73d
NingenTech <59954804+NingenTec@users.noreply.github.com>
2020-07-05 15:36:19 -0700

    Add post request to server and install body parser npm package

--- calculator.js
+++ calculator.js
@@ -5,7 +5,11 @@ const express = require("express");
 const app = express();
     
 app.get("/", function(_req, res){
-    res.send("Hello, world!");
+res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(_req, res){
+    res.send("Thanks for posting that!")
 });
 
 app.listen(3000, function(){
